88273cd8ad1cd30a2564c331d871e9854d2ac198
Kiran-Cooke <kmc72112@gmail.com>
2026-09-14 18:56:07 +0000

    Update

--- Java/000_Labs/006_doubles/base_code/starter.java
+++ Java/000_Labs/006_doubles/base_code/starter.java
@@ -9,6 +9,10 @@ class starter {
 	public static void main(String args[]) {
 		// the string "I love to learn coding remotely." will appear in
 		// the command window when you compile and run this program.
-		System.out.print("I love to learn coding remotely."); 
+
+		double F = 100;
+		double C = (F-32)*5/9;
+		System.out.println("If F="+F);
+		System.out.println("C="+C);
 	}
 }
--- Java/000_Labs/008_input/base_code/starter.java
+++ Java/000_Labs/008_input/base_code/starter.java
@@ -9,6 +9,11 @@ class starter {
 	public static void main(String args[]) {
 		// the string "I love to learn coding remotely." will appear in
 		// the command window when you compile and run this program.
-		System.out.print("I love to learn coding remotely."); 
+		import.java.until.Scanner
+		
+		
+		
+		
+		System.out.print("What is your name"); 
 	}
 }
