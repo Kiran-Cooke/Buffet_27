18fb4bcf71e1c37645261c1d00bf58ea650bd575
Kiran-Cooke <kmc72112@gmail.com>
2026-09-16 04:16:52 +0000

    commit

--- Java/101_Lecture_Notes/LectureInput.java
+++ Java/101_Lecture_Notes/LectureInput.java
@@ -1,9 +1,26 @@
 /*
     Lecture note example - Input!!
 */
+import java.until.Scanner;
 
 class LectureInput{
     public static void main(String args[]) {
         // Your Code Goes here!
-	}
+
+        double Chip = 2.99;
+        double Suger = 2.99;
+        double Rasin = 2.59;
+
+        System.out.println("Welcome to Cooke's Cookie shop");
+        System.out.println("1. Chocolate Chip Cookie - $"+Chip);
+        System.out.println("2. Suger Cookie - $"+Suger);
+        System.out.println("3. Rasin Cookie - $"+Rasin);
+
+        System.out.println("How many Chocolate Chip Cookies would you like");
+        Scanner sc = new Scanner(System.in);
+        int CCamount = sc.nextInt();
+        System.out.println("The Chocolate chips cost;"+sc )
+
+
+	} 
 }
